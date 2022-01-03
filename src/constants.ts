adc5c71ec0817b3bf3032dccabead2c1d09e332e
Ivan Ho <ivandaho@gmail.com>
2022-01-04 01:42:21 +0800

    Updates

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import moment from 'moment';
-import { Nakama } from './types/schedule';
-
-export const SPECIAL_CHORE_CYCLE_NAKAMAS = [Nakama.Ivan, Nakama.Gina, Nakama.Germ];
-/** the Monday of the start of the chore cycle */
-export const SPECIAL_CHORE_CYCLE_START_DATE = moment('2022-01-03');
